7fb5dfdb217c66576f684a2968a36fd10e79159a
Eric Meyer <eric.f.meyer@gmail.com>
2019-02-01 16:44:17 -0600

    Add a helper function.

--- chapter2/chapter_2.py
+++ chapter2/chapter_2.py
@@ -4,10 +4,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+def helper_decorate_plot(the_plot, title="", xlabel="", ylabel=""):
+    """Helper function to set title, x and y labels."""
+    the_plot.title(title)
+    the_plot.xlabel(xlabel)
+    the_plot.ylabel(ylabel)
+
+
 def first_plots():
     """Plot 4 points."""
     plt.plot([1, 3, 2, 4])
-    plt.title("Plotting single values")
+    helper_decorate_plot(plt, "Plotting single values")
     plt.show()
 
 
@@ -15,7 +22,7 @@ def quadratic_plot():
     """Plot a quadratic function."""
     values = range(6)
     plt.plot(values, [x ** 2 for x in values])
-    plt.title("Quadratic function")
+    helper_decorate_plot(plt, "Quadratic function")
     plt.show()
 
 
@@ -23,7 +30,7 @@ def better_quadratic_plot():
     """Plot a quadratic function using NumPy array."""
     values = np.arange(0.0, 6.0, 0.1)
     plt.plot(values, [x ** 2 for x in values])
-    plt.title("Better Quadratic function with NumPy arange()")
+    helper_decorate_plot(plt, "Better Quadratic function with NumPy arange()")
     plt.show()
 
 
@@ -33,7 +40,7 @@ def multiline_plots():
     plt.plot(values, [x * 1.5 for x in values],
              values, [x * 3 for x in values],
              values, [x / 3 for x in values])
-    plt.title("Multiple lines with normal range()")
+    helper_decorate_plot(plt, "Multiple lines with normal range()")
     plt.show()
 
 
@@ -43,7 +50,7 @@ def better_multiline_plots():
     plt.plot(values, values * 1.5,
              values, values * 3,
              values, values / 3)
-    plt.title("Multiple lines with NumPy arange()")
+    helper_decorate_plot(plt, "Multiple lines with NumPy arange()")
     plt.show()
 
 
@@ -54,11 +61,11 @@ def adding_a_grid():
              values, values * 3,
              values, values / 3)
     plt.grid(True)
-    plt.title("With a grid")
+    helper_decorate_plot(plt, "With a grid")
     plt.show()
 
 
-def setting_an_axis():
+def setting_an_axis_and_labels():
     """Plot on a figure with set axis limits."""
     values = np.arange(1, 5)
     plt.plot(values, values * 1.5,
@@ -66,7 +73,18 @@ def setting_an_axis():
              values, values / 3)
     print("Axis: {}".format(plt.axis()))
     plt.axis([0, 5, -1, 13])
-    plt.title("With Axis set")
+    helper_decorate_plot(plt, title="With Axis set",
+                         xlabel="This is the X axis",
+                         ylabel="This is the Y axis")
+    plt.show()
+
+
+def displaying_the_legend():
+    values = np.arange(1, 5)
+    plt.plot(values, values * 1.5, label='Normal')
+    plt.plot(values, values * 3, label='Fast')
+    plt.plot(values, values / 3, label='Slow')
+    plt.legend()
     plt.show()
 
 
@@ -77,4 +95,5 @@ if __name__ == '__main__':
     multiline_plots()
     better_multiline_plots()
     adding_a_grid()
-    setting_an_axis()
+    setting_an_axis_and_labels()
+    displaying_the_legend()
